e1b850d013196bb337c6ee47cafb4f839c5e499a
Luiz Pavanello <luizpavanello@gmail.com>
2021-08-16 10:04:27 -0300

    Arrow Function find() and findIndex().

--- es6_JavaScript/10_find()&findIndex().js
+++ es6_JavaScript/10_find()&findIndex().js
@@ -6,3 +6,18 @@
   → findIndex(): quite similar to find, but returns the index of the first element instead of the element itself.
 */
 
+const numArray = [ -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9] ;
+
+const resultFind = numArray.find(item => {
+  return true
+});
+
+console.log('With find we have the first element that matches:', resultFind)
+//-4 (first number in the array)
+
+const resultFindIndex = numArray.findIndex(item => {
+  return true
+});
+
+console.log('With findIndex we have the first element that matches:', resultFindIndex)
+//0 (first position of the array)
