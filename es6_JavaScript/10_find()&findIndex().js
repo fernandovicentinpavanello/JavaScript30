df05c88aaa85cd0dd0350e026f936056b059fc5e
Luiz Pavanello <luizpavanello@gmail.com>
2021-08-16 09:58:41 -0300

    Arrow Function find() and findIndex().

---
+++ es6_JavaScript/10_find()&findIndex().js
@@ -0,0 +1,8 @@
+/* Arrow Functions: find() and findIndex() 
+  → Search for elements inside the Array
+  
+  → find(): used to search for an element in the array that matches some condition. It returns the first element that matches the condition.
+
+  → findIndex(): quite similar to find, but returns the index of the first element instead of the element itself.
+*/
+
